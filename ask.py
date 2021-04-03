d5184c4331099107de821242df3849d867676001
jshoung314 <jshoung@andrew.cmu.edu>
2021-04-03 18:19:26 -0400

    pushing Yes for Mira

--- ask.py
+++ ask.py
@@ -48,7 +48,23 @@ class Generation():
 
         
     
+    # Mira's implementation that I'm pushing for her
     def generateYesQ(self):
+        auxi_verbs = ["am", "is", "are", "was", "were", "have", "has", "had", "do", "does", "did", "will", 
+            "would", "shall", "should", "may", "might", "must", "can", "could"]
+        def sentence_yes_questions(sentence):
+        result = []
+        for i in range(len(sentence)):
+            if (sentence[i] in auxi_verbs):
+                for j in range(i):
+                    result.append(sentence[j])
+            if (i < len(sentence)-1):
+                for k in range(i+1, len(sentence)-1):
+                    result.append(sentence[k])
+            result.insert(0, sentence[i][0].swapcase() + sentence[i][1:])
+            result.append("?")
+            return result
+    return result
 
     def generateNoQ(self):
 
