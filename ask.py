b27b3cac24a346473697d3d89c93bb0920bf977d
jshoung314 <jshoung@andrew.cmu.edu>
2021-04-04 21:02:18 -0400

    Naive implementation of recurisive WH question generation Currently does 0 recursions

--- ask.py
+++ ask.py
@@ -19,7 +19,7 @@ class Generation():
         # set of all the questions we generate (questions are in the form of strings)
         self.questions = set()
 
-    # Prints the preceeding part of a parse_tree
+    # Returns the preceeding part of a parse_tree
     def printPhraseBefore(self, parse_tree, index):
         phrase = ""
         if index == 0:
@@ -27,7 +27,7 @@ class Generation():
         for i in range(index-1):
             phrase = phrase + parse_tree[0][i] + " "
         return phrase
-    # Prints the successive part of a parse_tree
+    # Returns the successive part of a parse_tree
     # Someone please check my indexing - Jon
     def printPhraseAfter(self, parse_tree, index):
         phrase = ""
@@ -36,6 +36,40 @@ class Generation():
         for i in range(len(parse_tree[0])-index-1):
             phrase = " " + phrase + parse_tree[0][i+index+1]
         return phrase
+    # Returns a phrase with NP in index substituted for appropraite WH word
+    # If no NP found within indexed subtree, returns empty string
+    # Hope to eventually make this recursive:
+    # NOTE: current implementation only checks top level tree,
+    # TODO: implement recursion to check all levels of the tree for replacable NP
+    def printSubstituted(self, parse_tree, index):
+        # Get a subtree, check its label
+        subtree = parse_tree[0][index]
+        # (Replacable) noun phrase found
+        if subtree.label == 'NP':
+            # Find the entity type of the NP
+            doc = nlp(subtree.token)
+            # Hopefully these top level NP are fairly simple
+            # Guess every relevant WH word for substitution
+            for ent in doc.ents:
+                if ent.label_ == 'PERSON':
+                    # WH-word = who
+                    wh_word = "who"
+                elif ent.label_ == "DATE":
+                    # WH-word = when
+                    wh_word = "when"
+                elif ent.label == "TIME":
+                    # WH-word = when
+                    wh_word = "when"
+                elif ent.label_ == 'LOCATION':
+                    # WH-word = where
+                    wh_word = "where"
+                else:
+                    # WH-word = what
+                    wh_word = "what"
+            return wh_word
+        # NOTE: This else case should eventually be recursive
+        else:
+            return ""
     # Get WH questions
     def assignWHWord(self):
         verb_tags = {'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ'}
@@ -46,116 +80,25 @@ class Generation():
             parse_tree = rrp.parse_tagged(sentence)
             # Loop through the top layer of sentence
             for i in range(len(parse_tree[0])):
-                # Get a subtree, check its label
-                subtree = parse_tree[0][i]
-                # (Replacable) noun phrase found
-                if subtree.label == 'NP':
-                    # Get the rest of the sentence without this NP
+                # Get possible substitution for a NP in this index
+                wh_phrase = self.printSubstituted(parse_tree, i)
+                # Check for successful replacement
+                if (wh_phrase != ""):
+                    # Get the rest of the sentence and construct question
                     prev_phrase = self.printPhraseBefore(parse_tree, i)
                     next_phrase = self.printPhraseAfter(parse_tree, i)
-                    # Find the entity type of the NP
-                    doc = nlp(subtree.token)
-                    # Hopefully these top level NP are fairly simple
-                    # Guess every relevant WH word for substitution
-                     for ent in doc.ents:
-                        if ent.label_ == 'PERSON':
-                            # WH-word = who
-                            wh_word = "who"
-                        elif ent.label_ == "DATE":
-                            # WH-word = when
-                            wh_word = "when"
-                        elif ent.label == "TIME":
-                            # WH-word = when
-                            wh_word = "when"
-                        elif ent.label_ == 'LOCATION':
-                            # WH-word = where
-                            wh_word = "where"
-                        else:
-                            # WH-word = what
-                            wh_word = "what"
+                    question = prev_phrase + wh_phrase + next_phrase + " ?"
+                    self.questions.add(question)
 
-
-
-                    for verb_subtree in subtree:
+                    # Leftover from old implementation
+                    # for verb_subtree in subtree:
                         # verb found
-                        if verb_subtree.label in verb_tags:
+                        # if verb_subtree.label in verb_tags:
                             # now iterate over all noun phrases
-                            for j in range(len(parse_tree[0])):
-                                noun_subtree = parse_tree[0][j]
-                                if noun_subtree.label == 'NP':
-                                    # TODO: JUST TO HELP U JON:
-                                    # i : index of VP
-                                    # j : index of NP
-                                    # verb_subtree.token : the actual verb
-                                    # verb_subtree.label : the specific verb tag
-                                    # noun_subtree.token : the noun phrase
-                                    doc = nlp(noun_subtree.token)
-                                    # should have just 1 entity b/c we have just
-                                    # 1 noun phrase
-                                    # case whether VP comes before/after NP
-                                    if 0 < i and i < j:
-                                        # Get the phrase before this VP
-                                        prev_NP = parse_tree[0][i-1]
-                                        for ent in doc.ents:
-                                            if ent.label_ == 'PERSON':
-                                                # WH-word = who
-                                                question = subtree.token + " who?"
-                                                self.questions.add(question)
-                                            elif ent.label_ == "DATE":
-                                                # WH-word = when
-                                                question = subtree.token + " what date?"
-                                                self.questions.add(question)
-                                            elif ent.label == "TIME":
-                                                # WH-word = when
-                                                question = subtree.token + " what time?"
-                                                self.questions.add(question)
-                                            elif ent.label_ == 'LOCATION':
-                                                # WH-word = where
-                                                question = subtree.token + " where?"
-                                                self.questions.add(question)
-                                            else:
-                                                # WH-word = what
-                                                question = subtree.token + " what?"
-                                                self.questions.add(question)
-                                    else:
-                                        for ent in doc.ents:
-                                            if ent.label_ == 'PERSON':
-                                                # WH-word = who
-                                                question = "Who did" + noun_subtree.token + " " + verb_subtree.token + "?"
-                                                self.questions.add(question)
-                                            elif ent.label_ in ['DATE', 'TIME']:
-                                                # WH-word = when
-                                                self.questions.add()
-                                            elif ent.label_ == 'LOCATION':
-                                                # WH-word = where
-                                                self.questions.add()
-                                            else:
-                                                # WH-word = what
-                                                self.questions.add()
                                         # NOTE: for "why" and "how", we prob
                                         # need a way besides using NER. for
                                         # example detecting words like "because"
                     
-
-    # input is list of tuples returned by self.assignWHWord()
-    def generateWHWord(self, assignments):
-        # (verb, whword) = assignments
-        for (i in range(len(assignments))):
-            # read in the ith element of assignments
-            (verb, whword) = assignments[i]
-            # get the ith sentence corresponding to assignment, tagged
-            sentence = self.tagged[i]
-            # initialize blank list representing question
-            question = []
-            question.append(whword)
-            question.append(verb)
-            # now need a way to determine whether to use noun phrase from
-            # before or after the verb
-            # lazy implementation is just adding both
-
-
-        
-    
     auxi_verbs = ["am", "is", "are", "was", "were", "have", "has", 
     "had", "do", "does", "did", "will", "would", "shall", "should", 
     "may", "might", "must", "can", "could"]
